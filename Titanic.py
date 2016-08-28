aa6b05b229247c81d9b6b4707c0ba8c544c31583
chris george <chrisg16@hotmail.co.uk>
2016-08-28 23:27:50 +0100

    impute the missing values using mean & modes

--- Titanic.py
+++ Titanic.py
@@ -1,6 +1,9 @@
 ########### TITANIC KAGGLE COMPETITION ###########
 
 import pandas as pd
+import numpy as np
+import matplotlib as plt
+import math
 
 #read in train & test files
 Original_train = pd.read_csv("train.csv")
@@ -20,4 +23,40 @@ del test['Ticket']
 del train['Cabin']
 del test['Cabin']
 
-print hi
+#Have a look at the data and check for missing values
+train.info()
+print("############################")
+test.info()
+print("############################")
+
+#create a combined data set and find the means & modes of the attributes with missing values.
+#These will be used for imputation
+combined_data = pd.concat((train.iloc[:,1:8],test))
+combined_data.info()
+print("############################")
+age_mean = combined_data['Age'].mean()
+fare_mean = combined_data['Fare'].mean()
+embarked_mode = combined_data['Embarked'].mode()[0]
+
+#manual imputation for training set
+for i in range(len(train)):
+    if (pd.isnull(train.loc[i,'Age'])):
+        train.loc[i,'Age'] = age_mean
+    if (pd.isnull(train.loc[i,'Embarked'])):
+        train.loc[i,'Embarked'] = embarked_mode
+        
+#manual imputation for test set
+for i in range(len(test)):
+    if (pd.isnull(test.loc[i,'Age'])):
+        test.loc[i,'Age'] = age_mean
+    if (pd.isnull(test.loc[i,'Fare'])):
+        test.loc[i,'Fare'] = fare_mean
+
+
+        
+        
+
+
+
+
+
